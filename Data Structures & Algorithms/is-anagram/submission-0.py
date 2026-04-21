c320027d57767a365b42d1b7e2dd55c604cc7271
Vansh Kumar Nigam <151567282+niskarsh7@users.noreply.github.com>
2026-04-21 11:27:50 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,27 @@
+import array as arr
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s.lower())==sorted(t.lower()):
+            return True
+        
+        return False
+     
+
+        
+        
+        
+        
+
+        
+        
+ 
+
+            
+
+
+
+
+        
+
+
+        
